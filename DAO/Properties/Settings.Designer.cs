e7185db56bc0c30b30dc66af1014b111fa8498d8
Trieste96 <truongminhtriet96@gmail.com>
2016-10-21 17:40:08 +0700

    Đổi password thành sapwd

--- DAO/Properties/Settings.Designer.cs
+++ DAO/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace DAO.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("123456")]
+        [global::System.Configuration.DefaultSettingValueAttribute("sapwd")]
         public string password {
             get {
                 return ((string)(this["password"]));
